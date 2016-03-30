94359a48f773b5350f3a07134f35481fe063459c
Reyham <s3496982@student.rmit.edu.au>
2016-03-30 22:49:37 +1100

    -Added init_fave() to model class
    -init_fave() to return a HashMap.
    -Added comments to clarify on how a JSON is constructed and how a
    HashMap works.

--- model.java
+++ model.java
@@ -9,14 +9,14 @@ public class model {
 		Object jsonObject = parser.parse(source);
 		JSONArray listOfStates = (JSONArray)jsonObject;
 		
-		HashMap states = new HashMap();
+		HashMap states = new HashMap(); /*Declare outer HashMap: Key = state name, Value = HashMap stations*/
 		
 		for (int i=0; i < listOfStates.length(); i++) {
 			JSONObject state = (JSONObject)listOfStates.get(i);
 			String stateName = state.get("state");
 			JSONArray listOfCities = (JSONArray)state.get("stations");
 			
-			HashMap stations = new HashMap();
+			HashMap stations = new HashMap(); /*Declare inner HashMap: Key = city name, Value = URL string*/
 			
 			for (int j=0; j < listOfCities.length(); j++) {
 				JSONObject city = (JSONObject)listOfCities.get(j);
@@ -26,17 +26,68 @@ public class model {
 			}
 			states.put(stateName, stations);
 		}
+		
 		return states;
 	}
 	
 	public String getUrl(HashMap model, String stateName, String cityName){
 		String url;
-		url = model.get(0).get(stateName).get(cityName);
+		url = model.get(stateName).get(cityName);
 		return url;
 	}
 	
-	public void init_fave(){
+	/*
+	Converts JSON file containing favourites into HashMap.
+	e.g.
+	[
+		{
+			"city" : "middle of nowhere",
+			"state" : "somewhere in Tasmania"
+		},
+		{
+			"city" : "Melbourne",
+			"state" : "Victoria"
+		},
+		{
+			"city" : "potato",
+			"state" : "tomato"
+		}
+	]
+	This can be called an array of objects, anything within [] is called a JSONArray, and anything within {} is called a JSONObject.
+	So this would be a JSONArray filled with JSONObjects, whereas a file such as:
+	{
+		"city" : "middle of nowhere",
+		"state" : "somewhere in Tasmania"
+	}
+	Would only be called a JSONObject because there are no [], also:
+	{
+		[
+			{
+				"city" : "middle of nowhere",
+				"state" : "somewhere in Tasmania"
+			},
+			{
+				"city" : "Melbourne",
+				"state" : "Victoria"
+			}
+		]
+	}
+	Would mean it's a JSONObject that contains a JSONArray, and the JSONArray contains 2 JSONObjects.
+	Hope this helps, please delete this comment eventually.
+	*/
+	public void init_fave(JSONArray source){
+		JSONParser parser = parser.parse();
+		Object jsonObject = parser.parse(source);
+		JSONArray listOfFaves = (JSONArray)jsonObject;
 		
+		HashMap faves = new HashMap();/*Declare inner HashMap: Key = city name, Value = state name (This information needed to call getUrl() method.)*/
+		
+		for (int i=0; i < listOfFaves.length(); i++){
+			JSONObject fave = (JSONObject)listOfFaves.get(i);/*gets one JSON object, which is something within curly braces{}, from inside the JSON array[].*/
+			faves.put(fave.get("city"),fave.get("state"));
+		}
+		
+		return faves;
 	}
 	
 	public void addFave(Map<String, String>faveList, String cityName){
