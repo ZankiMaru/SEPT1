c70f3eb690a5fefff5ca93cc823cb5123a108170
Ben <s3494185@student.rmit.edu.au>
2016-04-05 16:06:12 +1000

    Added the getStationData() method to the Extraction class
    this method is used to get the weather data from the online json file
    for a station

--- Extraction.java
+++ Extraction.java
@@ -145,4 +145,49 @@ class Extraction {
 			e.printStackTrace();
 		}
 	}
+
+	public static JSONArray getStationData (String siteUrl)
+	{
+	    JSONParser parser = new JSONParser();
+        String text = "";
+        /* First, connect to the website using an url and pass the whole page
+         * as a string. Using StringBuilder to remove all the extra characters
+         * and new line character to compile the JSON.*/
+        try
+        {
+            URL oracle = new URL(siteUrl);
+            URLConnection yc = oracle.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+            String x = "";
+            StringBuilder inputLine = new StringBuilder();
+            
+            while ((x = in.readLine()) != null) {       
+                inputLine.append(x);
+            }   
+            text = inputLine.toString();
+        }
+        catch (MalformedURLException e1){
+            e1.printStackTrace();
+        } 
+        catch (IOException e){
+            e.printStackTrace();
+        }   
+      
+        /* Next is to parse the whole page string as a JSONObject and extract
+         * the needed data. In this example, it prints the name of the site 
+         * and the weather at every half hour. */
+        try{
+            Object obj = parser.parse(text);
+            JSONObject result = (JSONObject)obj;
+            JSONObject obs = (JSONObject) result.get("observations");
+            
+            JSONArray datas = (JSONArray) obs.get("data");
+            
+            return datas;
+            
+        }catch(ParseException pe){
+            System.out.println(pe);
+        }
+        return null;
+	}
 }
