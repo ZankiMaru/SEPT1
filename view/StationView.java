f662b809acd1ed1d945cb5ba41ef85e68df6ef7c
Ben <s3494185@student.rmit.edu.au>
2016-04-06 16:04:27 +1000

    Added the refresh and favourite buttons
    Add/remove favourite button isn't functional yet

--- view/StationView.java
+++ view/StationView.java
@@ -1,5 +1,10 @@
 package view;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
@@ -14,26 +19,25 @@ public class StationView extends JFrame
     JLabel lblStationName;
     NowPanel nowPanel;
     DayPanel dpToday, dpYesterday, dpDayBefore;
+    StationInfoPanel sipStation;
     
     
     public StationView(Station stationData)
     {
+        this.stationData = stationData;
         mainPanel = new JPanel();
-        lblStationName = new JLabel(stationData.name);
-        nowPanel = new NowPanel(stationData.getNow());
-        dpToday = new DayPanel("Today", stationData.getDay("today"));
-        dpYesterday = new DayPanel("Yesterday", stationData.getDay("yesterday"));
-        dpDayBefore = new DayPanel("Day Before", stationData.getDay("dayBefore"));
         
-        //this.setBounds(100, 100, 1000, 1000);
+        fillData();
+        
         this.setBounds(100, 100, 350, 700);
         
         this.setResizable(false);
         mainPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
         
         GridLayout mainLayout = new GridLayout(5, 1, 10, 10);
-        
-        mainPanel.add(lblStationName);
+
+        sipStation = new StationInfoPanel(this);
+        mainPanel.add(sipStation);
         mainPanel.add(nowPanel);
         mainPanel.add(dpToday);
         mainPanel.add(dpYesterday);
@@ -49,6 +53,62 @@ public class StationView extends JFrame
     
     public void fillData()
     {
+        nowPanel = new NowPanel(stationData.getNow());
+        dpToday = new DayPanel("Today", stationData.getDay("today"));
+        dpYesterday = new DayPanel("Yesterday", stationData.getDay("yesterday"));
+        dpDayBefore = new DayPanel("Day Before", stationData.getDay("dayBefore"));
+    }
+}
+
+class StationInfoPanel extends JPanel
+{
+    JPanel mainPanel, buttonsPanel;
+    JLabel lblStationLabel;
+    JButton btnRefresh, btnFavourite;
+    
+    public StationInfoPanel(StationView stationView)
+    {
+        GridLayout mainLayout = new GridLayout(2, 1, 0, 15);
+        FlowLayout buttonsLayout = new FlowLayout(FlowLayout.CENTER, 50, 0);
+        this.setBorder(BorderFactory.createLineBorder(Color.black));
         
+        mainPanel = new JPanel(mainLayout);
+        
+        //Buttons
+        buttonsPanel = new JPanel();
+        buttonsPanel.setLayout(buttonsLayout);
+        
+        btnFavourite = new JButton("Favourite");
+        btnFavourite.addActionListener(new ButtonListener(stationView));
+        btnRefresh = new JButton("Refresh");
+        btnRefresh.addActionListener(new ButtonListener(stationView));
+        
+        buttonsPanel.add(btnFavourite);
+        buttonsPanel.add(btnRefresh);
+        
+        //Station Name
+        lblStationLabel = new JLabel(stationView.stationData.name, SwingConstants.CENTER);
+        lblStationLabel.setFont(new Font("SansSerif", Font.PLAIN, 22));
+        
+        mainPanel.add(buttonsPanel);
+        mainPanel.add(lblStationLabel);
+        
+        this.add(mainPanel);
+    }
+}
+
+class ButtonListener implements ActionListener {
+    StationView stationView;
+    ButtonListener(StationView stationView) {
+        this.stationView = stationView;
+    }
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("Refresh")) {
+            //Refresh Data
+            stationView.fillData();
+        }
+        if (e.getActionCommand().equals("Favourite")) {
+            System.out.println("This will add/remove to the favourites list");
+        }
     }
 }
