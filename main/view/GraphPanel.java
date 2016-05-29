f60bfc9947d05b82e868e4c03c5ef37d98972eff
Ben <s3494185@student.rmit.edu.au>
2016-05-29 14:08:07 +1000

    Added buttons to graph
    Buttons are not yet functional

--- main/view/GraphPanel.java
+++ main/view/GraphPanel.java
@@ -1,5 +1,9 @@
 package main.view;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 import org.jfree.chart.*;
 import org.jfree.data.category.CategoryDataset;
@@ -12,14 +16,35 @@ import main.model.Station;
 public class GraphPanel extends JPanel{
 	
 	Station stationData;
+	JButton rdbTemp, rdbWind, rdbRain;
 	
 	public GraphPanel(Station stationData)
 	{
 		this.stationData = stationData;
+		this.setLayout(new BorderLayout());
+		
+		//Chart
 		JFreeChart chart = createChart(createDataset());
 		ChartPanel panel = new ChartPanel(chart);
-		this.add(panel);
+		this.add(panel, BorderLayout.CENTER);
+		
+		//Radio buttons
+		JPanel mainPanel = new JPanel(new FlowLayout());
+		rdbTemp = new JButton("Temperature");
+		rdbWind = new JButton("Wind");
+		rdbRain = new JButton("Rain");
+		rdbTemp.addActionListener(new GraphButtonListener(rdbTemp));
+		rdbWind.addActionListener(new GraphButtonListener(rdbTemp));
+		rdbRain.addActionListener(new GraphButtonListener(rdbTemp));
+		mainPanel.add(rdbTemp);
+		mainPanel.add(rdbWind);
+		mainPanel.add(rdbRain);
+
+		
+		
+		this.add(mainPanel, BorderLayout.PAGE_END);
 	}
+	
 	private CategoryDataset createDataset( ) 
 	{
 		DefaultCategoryDataset dataset = new DefaultCategoryDataset( );
@@ -41,4 +66,22 @@ public class GraphPanel extends JPanel{
 		 return chart;
 	   }
 
+}
+class GraphButtonListener implements ActionListener {
+    JButton button;
+    
+    public GraphButtonListener(JButton button) {
+        this.button = button;
+    }
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("Temperature")) {
+            
+        }
+        if (e.getActionCommand().equals("Wind")) {
+           
+        }
+        if (e.getActionCommand().equals("Rain")) {
+            
+        }
+    }
 }
