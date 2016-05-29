a6c223240ccaf812d25a261d7545a0a13b7690a3
Ben <s3494185@student.rmit.edu.au>
2016-05-29 16:09:17 +1000

    Fully Implemented Graph

--- main/view/GraphPanel.java
+++ main/view/GraphPanel.java
@@ -17,6 +17,9 @@ public class GraphPanel extends JPanel{
 	
 	Station stationData;
 	JButton rdbTemp, rdbWind, rdbRain;
+	JFreeChart chartActive, chartTemp, chartWind, chartRain;
+	JPanel redrawPanel;
+	ChartPanel panel;
 	
 	public GraphPanel(Station stationData)
 	{
@@ -24,18 +27,23 @@ public class GraphPanel extends JPanel{
 		this.setLayout(new BorderLayout());
 		
 		//Chart
-		JFreeChart chart = createChart(createDataset());
-		ChartPanel panel = new ChartPanel(chart);
-		this.add(panel, BorderLayout.CENTER);
+		chartTemp = createChart("Temperature", "Temp °C", createTempDataset());
+		chartWind = createChart("Wind", "Km/h", createWindDataset());
+		chartRain = createChart("Rain", "mm", createRainDataset());
 		
-		//Radio buttons
+		panel = new ChartPanel(chartTemp);
+		redrawPanel = new JPanel(new BorderLayout());
+		redrawPanel.add(panel, BorderLayout.CENTER);
+		this.add(redrawPanel, BorderLayout.CENTER);
+		
+		// Buttons
 		JPanel mainPanel = new JPanel(new FlowLayout());
 		rdbTemp = new JButton("Temperature");
 		rdbWind = new JButton("Wind");
 		rdbRain = new JButton("Rain");
-		rdbTemp.addActionListener(new GraphButtonListener(rdbTemp));
-		rdbWind.addActionListener(new GraphButtonListener(rdbTemp));
-		rdbRain.addActionListener(new GraphButtonListener(rdbTemp));
+		rdbTemp.addActionListener(new GraphButtonListener(this));
+		rdbWind.addActionListener(new GraphButtonListener(this));
+		rdbRain.addActionListener(new GraphButtonListener(this));
 		mainPanel.add(rdbTemp);
 		mainPanel.add(rdbWind);
 		mainPanel.add(rdbRain);
@@ -45,22 +53,35 @@ public class GraphPanel extends JPanel{
 		this.add(mainPanel, BorderLayout.PAGE_END);
 	}
 	
-	private CategoryDataset createDataset( ) 
+	private CategoryDataset createTempDataset() 
 	{
 		DefaultCategoryDataset dataset = new DefaultCategoryDataset( );
 		for (ForecastInterval inter: stationData.list2)
-		{
-			dataset.setValue( inter.temp, "Temp", inter.dateTime);  
-		}
+			dataset.setValue( inter.temp, "Temp °C", inter.dateTime);  
 		return dataset;         
 	}
-	 private JFreeChart createChart( CategoryDataset dataset )
+	private CategoryDataset createWindDataset() 
+	{
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset( );
+		for (ForecastInterval inter: stationData.list2)
+			dataset.setValue( inter.wind, "Km/h", inter.dateTime);  
+		return dataset;         
+	}
+	private CategoryDataset createRainDataset() 
+	{
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset( );
+		for (ForecastInterval inter: stationData.list2)
+			dataset.setValue( inter.rain, "mm", inter.dateTime);  
+		return dataset;         
+	}
+	
+	 private JFreeChart createChart(String title, String axis, CategoryDataset dataset )
 	 {
 		 
 		 JFreeChart chart = ChartFactory.createLineChart(      
-				 "Temperature", // chart title 
+				 title, // chart title 
 				 "Date",		// Bottom Axis label
-				 "Temp",
+				 axis,			// Right Axis label
 				 dataset);
 
 		 return chart;
@@ -68,20 +89,34 @@ public class GraphPanel extends JPanel{
 
 }
 class GraphButtonListener implements ActionListener {
-    JButton button;
-    
-    public GraphButtonListener(JButton button) {
-        this.button = button;
+	GraphPanel panel;
+    public GraphButtonListener(GraphPanel panel) {
+		this.panel = panel;
     }
     public void actionPerformed(ActionEvent e) {
         if (e.getActionCommand().equals("Temperature")) {
-            
+        	System.out.print("Temperature");
+        	panel.redrawPanel.removeAll();
+            panel.panel = new ChartPanel(panel.chartTemp);
+            panel.redrawPanel.add(panel.panel, BorderLayout.CENTER);
+            panel.redrawPanel.repaint();
+            panel.redrawPanel.revalidate();
         }
         if (e.getActionCommand().equals("Wind")) {
-           
+        	System.out.print("Wind");
+        	panel.redrawPanel.removeAll();
+            panel.panel = new ChartPanel(panel.chartWind);
+            panel.redrawPanel.add(panel.panel, BorderLayout.CENTER);
+            panel.redrawPanel.repaint();
+            panel.redrawPanel.revalidate();
         }
         if (e.getActionCommand().equals("Rain")) {
-            
+        	System.out.print("Rain");
+        	panel.redrawPanel.removeAll();
+            panel.panel = new ChartPanel(panel.chartRain);
+            panel.redrawPanel.add(panel.panel, BorderLayout.CENTER);
+            panel.redrawPanel.repaint();
+            panel.redrawPanel.revalidate();
         }
     }
 }
