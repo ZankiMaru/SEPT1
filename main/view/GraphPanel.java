cf5316e56608d842ae067a82d7c28de3912721c0
rmit-s3323595-yohanes-keanoe <s3323595@student.rmit.edu.au>
2016-05-29 21:06:18 +1000

    Fixed table

--- main/view/GraphPanel.java
+++ main/view/GraphPanel.java
@@ -63,9 +63,8 @@ public class GraphPanel extends JPanel{
 	{
 		DefaultCategoryDataset dataset = new DefaultCategoryDataset( );
 		for (ForecastInterval inter: stationData.list2){
-	       if(inter.getAMPM() == 0){
+	       if(inter.getAMPM() == 0)
 	          dataset.setValue( inter.temp, "Temp °C", Integer.toString(inter.getDate()) + ":" + Integer.toString(inter.getHour()) + "AM");  
-	       }
 	       else
 	          dataset.setValue( inter.temp, "Temp °C", Integer.toString(inter.getDate()) + ":" + Integer.toString(inter.getHour()) + "PM");  
 		}
@@ -74,17 +73,24 @@ public class GraphPanel extends JPanel{
 	private CategoryDataset createWindDataset() 
 	{
 		DefaultCategoryDataset dataset = new DefaultCategoryDataset( );
-		for (ForecastInterval inter: stationData.list2){
-			dataset.setValue( inter.wind, "Km/h", Integer.toString(inter.getDate()) + "-" + Integer.toString(inter.getHour())); 
-		}
+	    for (ForecastInterval inter: stationData.list2){
+          if(inter.getAMPM() == 0)
+             dataset.setValue( inter.wind, "Km/h", Integer.toString(inter.getDate()) + ":" + Integer.toString(inter.getHour()) + "AM");  
+          else
+             dataset.setValue( inter.wind, "Km/h", Integer.toString(inter.getDate()) + ":" + Integer.toString(inter.getHour()) + "PM");  
+      }
 		return dataset;         
 	}
 	private CategoryDataset createRainDataset() 
 	{
 		DefaultCategoryDataset dataset = new DefaultCategoryDataset( );
-		for (ForecastInterval inter: stationData.list2)
-			dataset.setValue( inter.rain, "mm", Integer.toString(inter.getDate()) + "/" + inter.getMonth() + ":" + Integer.toString(inter.getHour()));  
-		return dataset;         
+      for (ForecastInterval inter: stationData.list2){
+         if(inter.getAMPM() == 0)
+            dataset.setValue( inter.rain, "mm", Integer.toString(inter.getDate()) + ":" + Integer.toString(inter.getHour()) + "AM");  
+         else
+            dataset.setValue( inter.rain, "mm", Integer.toString(inter.getDate()) + ":" + Integer.toString(inter.getHour()) + "PM");  
+      }	
+      return dataset;         
 	}
 	
 	 private JFreeChart createChart(String title, String axis, CategoryDataset dataset )
@@ -112,7 +118,7 @@ class GraphButtonListener implements ActionListener {
     }
     public void actionPerformed(ActionEvent e) {
         if (e.getActionCommand().equals("Temperature")) {
-        	System.out.print("Temperature");
+//        	System.out.print("Temperature");
         	panel.redrawPanel.removeAll();
             panel.panel = new ChartPanel(panel.chartTemp);
             panel.redrawPanel.add(panel.panel, BorderLayout.CENTER);
@@ -120,7 +126,7 @@ class GraphButtonListener implements ActionListener {
             panel.redrawPanel.revalidate();
         }
         if (e.getActionCommand().equals("Wind")) {
-        	System.out.print("Wind");
+//        	System.out.print("Wind");
         	panel.redrawPanel.removeAll();
             panel.panel = new ChartPanel(panel.chartWind);
             panel.redrawPanel.add(panel.panel, BorderLayout.CENTER);
@@ -128,7 +134,7 @@ class GraphButtonListener implements ActionListener {
             panel.redrawPanel.revalidate();
         }
         if (e.getActionCommand().equals("Rain")) {
-        	System.out.print("Rain");
+//        	System.out.print("Rain");
         	panel.redrawPanel.removeAll();
             panel.panel = new ChartPanel(panel.chartRain);
             panel.redrawPanel.add(panel.panel, BorderLayout.CENTER);
